c9caee95adfc77bce154d81d27175e331a344f43
Csabbah <csabbah@ryerson.ca>
2022-05-01 17:32:01 -0400

    Populated connection.js with code to connect to database & included initial setup in all .sql files (minus seeds.sql)

---
+++ db/db.sql
@@ -0,0 +1,5 @@
+-- Drop the database ONLY if it exists
+DROP DATABASE IF EXISTS company;
+
+CREATE DATABASE company;
+USE company;
---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS job_role;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY, -- department id
+  dep_name VARCHAR(30) NOT NULL -- department name
+);
+
+CREATE TABLE job_role (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY, -- role id
+  title VARCHAR(30) NOT NULL, -- role title
+  salary DECIMAL NOT NULL, -- role salary
+  department_id INTEGER, -- references the department associated with role 
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY, -- employee id
+  first_name VARCHAR(30) NOT NULL, -- employee first name
+  last_name DECIMAL NOT NULL, -- employee last name
+  role_id INTEGER, -- references emeployee role 
+  manager_id INTEGER NULL, -- references the manager associated with employee 
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES job_role(id) ON DELETE SET NULL
+);
