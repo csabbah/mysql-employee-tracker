1a1c23ad3da059d89f01cce5699cf3c3c9d185b8
Csabbah <csabbah@ryerson.ca>
2022-05-02 11:59:52 -0400

    Cleaned up reptitive code - Created a function that is now used in all conditionals and returns the queried SQL data

--- index.js
+++ index.js
@@ -29,50 +29,34 @@ const promptOptions = () => {
   ]);
 };
 
+function checkResults(sql) {
+  db.query(sql, (err, rows) => {
+    if (err) {
+      console.log({ error: err.message });
+      return;
+    }
+    console.log({
+      message: 'success',
+      data: rows,
+    });
+    process.exit(); // Terminate command line after returning data
+  });
+}
+
 promptOptions().then((selectedOption) => {
   const { optionPicked } = selectedOption; // Extract the value from the object
 
   // Execute the conditionals to return the appropriate data
   if (optionPicked == 'View all departments') {
     const sql = `SELECT * FROM department`; // Select all data from the department table
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).console.log({ error: err.message });
-        return;
-      }
-      console.log({
-        message: 'success',
-        data: rows,
-      });
-      process.exit(); // Terminate command line after returning data
-    });
+    checkResults(sql);
   }
   if (optionPicked == 'View all roles') {
     const sql = `SELECT * FROM role`;
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).console.log({ error: err.message });
-        return;
-      }
-      console.log({
-        message: 'success',
-        data: rows,
-      });
-      process.exit();
-    });
+    checkResults(sql);
   }
   if (optionPicked == 'View all employees') {
     const sql = `SELECT * FROM employee`;
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).console.log({ error: err.message });
-        return;
-      }
-      console.log({
-        message: 'success',
-        data: rows,
-      });
-      process.exit();
-    });
+    checkResults(sql);
   }
 });
